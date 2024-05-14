482af78c5b4ddb0dbb388e707db11ae2826bc41f
Ebalo55 <12586380+ebalo55@users.noreply.github.com>
2024-05-14 07:17:09 +0200

    feat: server cli basic arguments definition

---
+++ server/src/cli_interface.rs
@@ -0,0 +1,35 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug, PartialEq)]
+#[command(version, about, long_about = None)]
+pub struct CliArguments {
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count, global = true)]
+    pub debug: u8,
+
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+/// First level server commands
+#[derive(Subcommand, Debug, PartialEq)]
+pub enum Commands {
+    /// Compile agent or C2 gui
+    Compile(CompileArguments),
+}
+
+/// Compilation arguments
+#[derive(Debug, PartialEq)]
+pub struct CompileArguments {
+    #[command(subcommand)]
+    pub command: CompileSubcommands,
+}
+
+/// Compilation subcommands
+#[derive(Subcommand, Debug, PartialEq)]
+pub enum CompileSubcommands {
+    /// Compile the agent
+    Agent,
+    /// Compile the C2 GUI
+    Gui,
+}
