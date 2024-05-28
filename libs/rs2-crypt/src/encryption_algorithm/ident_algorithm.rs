113526c9708c67f46c7265f4c00472f05d963477
ebalo <ebalo55@users.noreply.github.com>
2024-05-28 07:15:47 +0200

    feat: asymmetric encryption algorithm

--- libs/rs2-crypt/src/encryption_algorithm/ident_algorithm.rs
+++ libs/rs2-crypt/src/encryption_algorithm/ident_algorithm.rs
@@ -5,18 +5,54 @@ use hkdf::{Hkdf, HmacImpl};
 #[cfg(feature = "hkdf")]
 use hkdf::hmac::digest::OutputSizeUser;
 
+use crate::symmetric_encryption_algorithm::SymmetricEncryptionAlgorithm;
+
 use super::EncryptionAlgorithm;
+#[cfg(feature = "hkdf")]
+use super::WithKeyDerivation;
 
 /// An encryptor that does not encrypt or decrypt data.
 #[derive(Clone)]
 pub struct IdentEncryptor;
+
+impl SymmetricEncryptionAlgorithm for IdentEncryptor {
+	fn set_nonce(&mut self, _nonce: Bytes) -> Result<&mut Self> {
+		Ok(self)
+	}
+
+	fn set_key(&mut self, _key: Bytes) -> Result<&mut Self> {
+		Ok(self)
+	}
+
+	fn make_nonce(&mut self) -> &mut Self {
+		self
+	}
+
+	fn make_key(&mut self) -> &mut Self {
+		self
+	}
+
+	fn get_nonce(&self) -> Bytes {
+		Bytes::new()
+	}
+
+	fn get_key(&self) -> Bytes {
+		Bytes::new()
+	}
+}
+
+impl From<Bytes> for IdentEncryptor {
+	fn from(_value: Bytes) -> Self {
+		Self
+	}
+}
 
 impl EncryptionAlgorithm for IdentEncryptor {
 	fn encrypt(&mut self, data: Bytes) -> Result<Bytes> {
 		Ok(data.clone())
 	}
 
-	fn decrypt(&self, data: Bytes) -> Result<Bytes> {
+	fn decrypt(&self, data: Bytes, _key: Option<Bytes>) -> Result<Bytes> {
 		Ok(data)
 	}
 
@@ -24,15 +60,13 @@ impl EncryptionAlgorithm for IdentEncryptor {
 		Self
 	}
 
-	fn from(_key: Bytes) -> Self {
-		Self
+	fn make_key(&mut self) -> Result<&mut Self> {
+		Ok(self)
 	}
+}
 
-	fn make_key(&mut self) -> &mut Self {
-		self
-	}
-
-	#[cfg(feature = "hkdf")]
+#[cfg(feature = "hkdf")]
+impl WithKeyDerivation for IdentEncryptor {
 	fn derive_key<H, I>(&mut self, _hkdf: Hkdf<H, I>) -> Result<&Self>
 		where H: OutputSizeUser,
 		      I: HmacImpl<H> {
