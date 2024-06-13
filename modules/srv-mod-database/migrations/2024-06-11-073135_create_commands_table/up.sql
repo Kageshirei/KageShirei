b15a81935943b22f11054fb14a215cd39fd183c9
ebalo55 <ebalo55@users.noreply.github.com>
2024-06-13 06:09:47 +0200

    feat: commands update to insert a sequence counter relative to the session

--- modules/srv-mod-database/migrations/2024-06-11-073135_create_commands_table/up.sql
+++ modules/srv-mod-database/migrations/2024-06-11-073135_create_commands_table/up.sql
@@ -1,19 +1,49 @@
 -- Your SQL goes here
-create table if not exists commands (
-    id          varchar(32) primary key,
-    ran_by      varchar(32) not null references users (id),
-    command     text        not null,
+create table if not exists commands
+(
+    id               varchar(32) primary key,
+    ran_by           varchar(32) not null references users (id),
+    command          text        not null,
     -- The session_id is a foreign key that references the agents table or a static value defined to "global" if the command
     -- was ran in the context of the global terminal emulator.
-    session_id  varchar(32) not null,
-    output      text,
-    exit_code   integer,
+    session_id       varchar(32) not null,
+    output           text,
+    exit_code        integer,
+    -- The sequence_counter is a unique identifier for the command within the session_id. It is used to order the commands
+    -- in the order they were ran.
+    sequence_counter bigint,
     -- Soft delete for the `clear` command.
-    deleted_at  timestamptz,
+    deleted_at       timestamptz,
     -- Restore timestamp for the `history` command.
-    restored_at timestamptz,
-    created_at  timestamptz not null default current_timestamp,
-    updated_at  timestamptz not null default current_timestamp
+    restored_at      timestamptz,
+    created_at       timestamptz not null default current_timestamp,
+    updated_at       timestamptz not null default current_timestamp,
+    unique (session_id, sequence_counter)
 );
 
 create index if not exists idx_commands_session_id on commands (session_id);
+
+create or replace function set_commands_sequence_counter()
+    returns trigger as
+$$
+declare
+    current_max bigint;
+begin
+    -- Get the current max session_command_id for this session_id
+    select coalesce(max(sequence_counter), -1) + 1
+    into current_max
+    from commands
+    where session_id = new.session_id;
+
+    -- Set the session_command_id for the new row
+    new.sequence_counter = current_max;
+
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger before_insert_commands
+    before insert
+    on commands
+    for each row
+execute function set_commands_sequence_counter();
