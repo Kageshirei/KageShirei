517c2379a90047e28c4f786892021943a866d610
Ebalo <12586380+ebalo55@users.noreply.github.com>
2024-10-16 15:42:21 +0200

    refactor: linting

--- libs/kageshirei-indirect-syscall/src/lib.rs
+++ libs/kageshirei-indirect-syscall/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
-/// A crate for performing indirect syscalls on x86 and x86_64 architectures.
-/// It provides macros and functions to execute syscalls with dynamic syscall numbers and addresses.
+//! A crate for performing indirect syscalls on x86 and x86_64 architectures.
+//! It provides macros and functions to execute syscalls with dynamic syscall numbers and addresses.
 pub mod syscall;
 
 pub use syscall::do_syscall;
