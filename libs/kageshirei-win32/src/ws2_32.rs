12be64352ab5791a5a6eca07b6b3bc8649cea96a
Ebalo <12586380+ebalo55@users.noreply.github.com>
2024-10-16 17:42:22 +0200

    fix: linting

--- libs/kageshirei-win32/src/ws2_32.rs
+++ libs/kageshirei-win32/src/ws2_32.rs
@@ -1,6 +1,11 @@
 pub type SOCKET = usize;
-// Data structures for Winsock
+
+/// The WSADATA structure contains information about the Windows Sockets implementation.
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/ns-winsock2-wsadata
 #[repr(C)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
 pub struct WsaData {
     pub w_version:        u16,
     pub w_high_version:   u16,
@@ -11,7 +16,12 @@ pub struct WsaData {
     pub lp_vendor_info:   *mut i8,
 }
 
+/// The SOCKADDR_IN structure specifies a transport address and port for the AF_INET address family.
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/ws2def/ns-ws2def-sockaddr_in
 #[repr(C)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
 pub struct SockAddrIn {
     pub sin_family: u16,
     pub sin_port:   u16,
@@ -19,18 +29,33 @@ pub struct SockAddrIn {
     pub sin_zero:   [i8; 8],
 }
 
+/// The in_addr structure represents an IPv4 Internet address.
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/ns-winsock2-in_addr
 #[repr(C)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
 pub struct InAddr {
     pub s_addr: u32,
 }
 
+/// The sockaddr structure varies depending on the protocol selected.
+///
+/// Except for the sin*_family parameter, sockaddr contents are expressed in network byte order.
 #[repr(C)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
 pub struct SockAddr {
     pub sa_family: u16,
     pub sa_data:   [i8; 14],
 }
 
+/// The addrinfo structure is used by the getaddrinfo function to hold host address information.
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/ws2def/ns-ws2def-addrinfoa
 #[repr(C)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
 pub struct AddrInfo {
     pub ai_flags:     i32,
     pub ai_family:    i32,
@@ -42,41 +67,137 @@ pub struct AddrInfo {
     pub ai_next:      *mut AddrInfo,
 }
 
+/// The FD_SET structure contains an array of sockets to be checked for a particular condition.
+///
+/// The fd_set structure is used by various Windows Sockets functions and service providers, such as
+/// the select function, to place sockets into a "set" for various purposes, such as testing a given
+/// socket for readability using the readfds parameter of the select function.
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/ns-winsock2-fd_set
 #[repr(C)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
 pub struct FD_SET {
     pub fd_count: u32,
     pub fd_array: [SOCKET; 64],
 }
 
+/// The timeval structure is used to specify a time interval.
+///
+/// It is associated with the Berkeley Software Distribution (BSD) Time.h header file.
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/ns-winsock2-timeval
 #[repr(C)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Eq, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
 pub struct TIMEVAL {
     pub tv_sec:  i32,
     pub tv_usec: i32,
 }
 
-// Define function types for Winsock functions
-
+/// The WSAStartup function initiates use of the Winsock DLL by a process.
 ///
+/// int WSAAPI WSAStartup(
+///   [in]  WORD      wVersionRequested,
+///   [out] LPWSADATA lpWSAData
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-wsastartup
 type WSAStartupFunc = unsafe extern "system" fn(wVersionRequested: u16, lpWsaData: *mut WsaData) -> i32;
+
+/// The WSACleanup function terminates use of the Winsock 2 DLL (Ws2_32.dll).
 ///
+/// int WSAAPI WSACleanup();
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-wsacleanup
 type WSACleanupFunc = unsafe extern "system" fn() -> i32;
+
+/// The socket function creates a socket that is bound to a specific transport service provider.
 ///
+/// SOCKET WSAAPI socket(
+///   [in] int af,
+///   [in] int type,
+///   [in] int protocol
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
 type SocketFunc = unsafe extern "system" fn(af: i32, socket_type: i32, protocol: i32) -> SOCKET;
+
+/// The connect function establishes a connection to a specified socket.
 ///
+/// int WSAAPI connect(
+///   [in] SOCKET         s,
+///   [in] const sockaddr *name,
+///   [in] int            namelen
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-connect
 type ConnectFunc = unsafe extern "system" fn(s: SOCKET, name: *const SockAddr, namelen: i32) -> i32;
+
+/// The send function sends data on a connected socket.
 ///
+/// int WSAAPI send(
+///   [in] SOCKET     s,
+///   [in] const char *buf,
+///   [in] int        len,
+///   [in] int        flags
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-send
 type SendFunc = unsafe extern "system" fn(s: SOCKET, buf: *const i8, len: i32, flags: i32) -> i32;
+
+/// The recv function receives data from a connected socket or a bound connectionless socket.
 ///
+/// int WSAAPI recv(
+///   [in]  SOCKET s,
+///   [out] char   *buf,
+///   [in]  int    len,
+///   [in]  int    flags
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-recv
 type RecvFunc = unsafe extern "system" fn(s: SOCKET, buf: *mut i8, len: i32, flags: i32) -> i32;
+
+/// The closesocket function closes an existing socket.
 ///
-type ClosesocketFunc = unsafe extern "system" fn(s: SOCKET) -> i32;
+/// int closesocket(
+///   [in] SOCKET s
+/// );
 ///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-closesocket
+type CloseSocketFunc = unsafe extern "system" fn(s: SOCKET) -> i32;
+
+/// The inet_addr function converts a string containing an IPv4 dotted-decimal address into a proper
+/// address for the IN_ADDR structure.
+///
+/// unsigned long WSAAPI inet_addr(
+///   const char *cp
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-inet_addr
 type InetAddrFunc = unsafe extern "system" fn(cp: *const i8) -> u32;
+
+/// The htons function converts a u_short from host to TCP/IP network byte order (which is
+/// big-endian).
 ///
+/// u_short htons(
+///   [in] u_short hostshort
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-htons
 type HtonsFunc = unsafe extern "system" fn(hostshort: u16) -> u16;
+
+/// The getaddrinfo function provides protocol-independent translation from an ANSI host name to an
+/// address.
 ///
+/// INT WSAAPI getaddrinfo(
+///   [in, optional] PCSTR           pNodeName,
+///   [in, optional] PCSTR           pServiceName,
+///   [in, optional] const ADDRINFOA *pHints,
+///   [out]          PADDRINFOA      *ppResult
+/// );
+///
+/// https://learn.microsoft.com/en-us/windows/win32/api/ws2tcpip/nf-ws2tcpip-getaddrinfo
 type GetAddrInfoFunc = unsafe extern "system" fn(
     node: *const i8,
     service: *const i8,
@@ -141,7 +262,7 @@ pub struct Winsock {
     pub connect:            Option<ConnectFunc>,
     pub send:               Option<SendFunc>,
     pub recv:               Option<RecvFunc>,
-    pub closesocket:        Option<ClosesocketFunc>,
+    pub closesocket:        Option<CloseSocketFunc>,
     pub inet_addr:          Option<InetAddrFunc>,
     pub htons:              Option<HtonsFunc>,
     pub getaddrinfo:        Option<GetAddrInfoFunc>,
