012f9fefa64d8ad63faea9d2854a34fbe33ebe98
Ebalo <12586380+ebalo55@users.noreply.github.com>
2024-12-02 06:19:44 +0100

    refactor: linting

--- command-and-control-gui/src-tauri/src/main.rs
+++ command-and-control-gui/src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(clippy::multiple_crate_versions, reason = "required by tauri")]
+#![allow(clippy::print_stderr, reason = "required for error handling")]
 
 fn main() {
     let result = tauri::Builder::default().run(tauri::generate_context!());
